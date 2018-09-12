13b0ce2bd824e7f696fe999908c86a9e79e0afac
Mariana <marianac94@hotmail.com>
2018-09-12 10:26:46 -0500

    Commit 9 - Created loops.js file

--- hw9-11/reume_page/loop-questions/app.js
+++ hw9-11/reume_page/loop-questions/app.js
@@ -3,7 +3,41 @@
 // for (var i = 0; i <= 99; i++) {
 //   console.log(i);
 // }
+//
+// let num = 99;
+// while(num >= 0) {
+//   console.log(num);
+//   num--;
+// }
+//
+// let num = 298;
+// while (num >= 1) {
+//   console.log(num);
+//   num -= 3;
+// }
+//
+// let num = 0;
+// while (num <= 98) {
+//   console.log(num);
+//   num += 2;
+// }
+//
+// let num = 98;
+// while (num >= 0) {
+//   console.log(num);
+//   num -= 2;
+// }
 
-for (var i = 0; i >= 99; i++) {
-  console.log(i);
+// for (var i = 49; i <= 72; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 81; i >= 26; i--) {
+//   console.log(i);
+// }
+
+let num = 1;
+while (num <= 91) {
+  console.log(num);
+  num *= 3;
 }
