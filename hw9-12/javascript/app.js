f0d72f3720d5f2d908be7815e33502be457d026a
Mariana <marianac94@hotmail.com>
2018-09-13 09:45:39 -0500

    Commit 6 - Yell at the Ninja Turtles answered

--- hw9-12/javascript/app.js
+++ hw9-12/javascript/app.js
@@ -45,37 +45,50 @@
 // }
 //
 //
-5. Getting to know you
-Use the following arrays to answer the questions below (name, age, hometown):
+// 5. Getting to know you
+// Use the following arrays to answer the questions below (name, age, hometown):
+//
+// const shahzad = ["Shahzad", 1000, "Austin"];
+// const jim = ["Jim", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const daniel = ["Daniel", 186, "Dallas"];
+// const ryan = ["Ryan", 65, "Denver"];
+//
+// Daniel decides that Shahzad can't be named "Shahzad" anymore. Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
+//     var remove = shahzad.splice(0, 1, 'Gameboy');
+//       console.log(shahzad);
+//
+// Daniel just had his birthday; change Daniel's array to reflect him being a year older. Don't just hard code 187--pretend that you don't know that his age is 186, and write your code to just make him a year older than whatever age he was.
+//         daniel[1] += 1
+//           console.log(daniel);
+//
+//
+// Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+//         daniel[2] = 'Gotham City'
+//             console.log(daniel);
+//
+// Reuben left Durham 5 years ago to come to Chicago. Remove "Durham" from Reuben's array and add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+//       var remove = reuben.splice(2, 2, 'Chicago');
+//         console.log(reuben)
+//
+// Jim could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
+//         jim.pop("All cities");
+//         jim.push("Chicago", "SF", "Mexico")
+//           console.log(jim);
+//
+// Bonus: Whoops! Jim is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim is now, remove it from the array using .splice()
+//           var remove = jim.splice(4)
+//             console.log(jim);
 
-const shahzad = ["Shahzad", 1000, "Austin"];
-const jim = ["Jim", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const daniel = ["Daniel", 186, "Dallas"];
-const ryan = ["Ryan", 65, "Denver"];
 
-Daniel decides that Shahzad can't be named "Shahzad" anymore. Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
-    var remove = shahzad.splice(0, 1, 'Gameboy');
-      console.log(shahzad);
+6. Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
 
-Daniel just had his birthday; change Daniel's array to reflect him being a year older. Don't just hard code 187--pretend that you don't know that his age is 186, and write your code to just make him a year older than whatever age he was.
-        daniel[1] += 1
-          console.log(daniel);
+Use a for loop to call .toUpperCase() on each of them and print out the result.
 
+Bonus: Modify the answer you just wrote. Instead of all letters being uppercase, make the letters alternate back and forth between uppercase and lowercase.
 
-Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
-        daniel[2] = 'Gotham City'
-            console.log(daniel);
+const ninjaTurtles = ['donatello', 'leonardo', 'raphael', 'michaelangelo'];
 
-Reuben left Durham 5 years ago to come to Chicago. Remove "Durham" from Reuben's array and add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
-      var remove = reuben.splice(2, 2, 'Chicago');
-        console.log(reuben)
-
-Jim could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
-        jim.pop("All cities");
-        jim.push("Chicago", "SF", "Mexico")
-          console.log(jim);
-
-Bonus: Whoops! Jim is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim is now, remove it from the array using .splice()
-          var remove = jim.splice(4)
-            console.log(jim);
+for(let i = 0; i < ninjaTurtles.length; i++){
+  ninjaTurtles.map(function(ninjaTurtles) { return ninjaTurtles.toUpperCase()})
+}
