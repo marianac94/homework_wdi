cddff2ffec97bcc9ba103c6560cc8d40492e2a00
Mariana <marianac94@hotmail.com>
2018-09-13 08:41:03 -0500

    Commit 3 - Excited Kittens answered

--- hw9-12/javascript/app.js
+++ hw9-12/javascript/app.js
@@ -1,10 +1,18 @@
 console.log('Im working!')
 
-// for(let i = 1; i <= 20; i++){
+for(let i = 1; i <= 20; i++){
+  console.log(i);
+}
+
+
+// for (let i = 0; i <= 200; i+=2){
 //   console.log(i);
 // }
 
 
-for (let i = 0; i <= 200; i+=2){
-  console.log(i);
-}
+// for (let i = 0; i <= 20; i++){
+//   if (i % 0) {
+//     console.log('');
+//   }
+//   console.log('Love me, pet me! HSSSSSS!');
+// }
