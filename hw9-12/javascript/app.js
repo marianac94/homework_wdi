39090cbb583617490669012fefb170149e833c9f
Mariana <marianac94@hotmail.com>
2018-09-13 09:37:13 -0500

    Commit 5 - Getting to Know You answered

--- hw9-12/javascript/app.js
+++ hw9-12/javascript/app.js
@@ -28,18 +28,54 @@
 // };
 //
 //
-4. NOTE: THIS IS A CLASSIC RITE-OF-PASSAGE FOR JAVASCRIPT PROGRAMMERS, ESPECIALLY ONES AT GA
----
-Write a javascript application that logs all numbers from 1 - 100.
-If a number is divisible by 3 log "Fizz" instead of the number.
-If a number is divisible by 5 log "Buzz" instead of the number.
-If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
-for(let i = 1; i <= 100; i++){
-console.log(i);
-    if(i % 3 === 0) {
-    console.log('Fizz');
-    } else if(i % 5 === 0) {
-      console.log('Buzz');
-      } else if(3 % i && 5 % i === 0)
-        console.log('FizzBuz');
-}
+// 4. NOTE: THIS IS A CLASSIC RITE-OF-PASSAGE FOR JAVASCRIPT PROGRAMMERS, ESPECIALLY ONES AT GA
+// ---
+// Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+// for(let i = 1; i <= 100; i++){
+// console.log(i);
+//     if(i % 3 === 0) {
+//     console.log('Fizz');
+//     } else if(i % 5 === 0) {
+//       console.log('Buzz');
+//       } else if(3 % i && 5 % i === 0)
+//         console.log('FizzBuz');
+// }
+//
+//
+5. Getting to know you
+Use the following arrays to answer the questions below (name, age, hometown):
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+Daniel decides that Shahzad can't be named "Shahzad" anymore. Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
+    var remove = shahzad.splice(0, 1, 'Gameboy');
+      console.log(shahzad);
+
+Daniel just had his birthday; change Daniel's array to reflect him being a year older. Don't just hard code 187--pretend that you don't know that his age is 186, and write your code to just make him a year older than whatever age he was.
+        daniel[1] += 1
+          console.log(daniel);
+
+
+Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+        daniel[2] = 'Gotham City'
+            console.log(daniel);
+
+Reuben left Durham 5 years ago to come to Chicago. Remove "Durham" from Reuben's array and add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+      var remove = reuben.splice(2, 2, 'Chicago');
+        console.log(reuben)
+
+Jim could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
+        jim.pop("All cities");
+        jim.push("Chicago", "SF", "Mexico")
+          console.log(jim);
+
+Bonus: Whoops! Jim is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim is now, remove it from the array using .splice()
+          var remove = jim.splice(4)
+            console.log(jim);
