6ab79ef51d855bc30439b907ab91888f2b938b31
Mariana <marianac94@hotmail.com>
2018-09-15 08:54:50 -0500

    C. Booleans

--- hw9-14/js/app.js
+++ hw9-14/js/app.js
@@ -22,30 +22,50 @@ console.log('weekends rock!');
 //   --> Is a 75% - 90% planning and a 10% - 25% typing the problem and start writing it down.
 
 
-B. Strings
-Create a variable called firstVariable.
-var = firstVariable{};
+// B. Strings
+// Create a variable called firstVariable.
+// var = firstVariable{};
+//
+// Assign it the value of the string "Hello World"
+// let firstVariable = "Hello World";
+// console.log(firstVariable);
+//
+// Change the value of this variable to some number.
+// firstVariable = 80;
+// console.log(firstVariable);
+//
+// Store the value of firstVariable in a new variable called secondVariable
+// let secondVariable = 80;
+// console.log(secondVariable);
+//
+// Change the value of secondVariable to any string.
+// secondVariable = 'newString';
+// console.log(secondVariable);
+//
+// What is the value of firstVariable?
+// --> The value is 80.
+// console.log(firstVariable);
+//
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+// let yourName = 'Mariana';
+// console.log('Hello my name is ' + yourName);
 
-Assign it the value of the string "Hello World"
-let firstVariable = "Hello World";
-console.log(firstVariable);
 
-Change the value of this variable to some number.
-firstVariable = 80;
-console.log(firstVariable);
+// C. Booleans
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console.
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
 
-Store the value of firstVariable in a new variable called secondVariable
-let secondVariable = 80;
-console.log(secondVariable);
-
-Change the value of secondVariable to any string.
-secondVariable = 'newString';
-console.log(secondVariable);
-
-What is the value of firstVariable?
---> The value is 80.
-console.log(firstVariable);
-
-Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
-let yourName = 'Mariana';
-console.log('Hello my name is ' + yourName);
+  console.log(a < b);
+  console.log(c != d);
+  console.log('Name' === 'Name');
+  console.log(true >= false);
+  console.log(false == false == false == false == false == false == true);
+  console.log(false === false)
+  console.log(e === 'Kevin');
+  console.log(a < b <= c);
+  console.log(a === a != d);
+  console.log(48 == '48');
