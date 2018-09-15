9ca800d7ac1eaa43e6e865b84452ab379db83878
Mariana <marianac94@hotmail.com>
2018-09-15 09:09:33 -0500

    D. The farm

--- hw9-14/js/app.js
+++ hw9-14/js/app.js
@@ -53,19 +53,43 @@ console.log('weekends rock!');
 
 // C. Booleans
 // Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console.
-const a = 4;
-const b = 53;
-const c = 57;
-const d = 16;
-const e = 'Kevin';
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
+//
+//   console.log(a < b);
+//   console.log(c != d);
+//   console.log('Name' === 'Name');
+//   console.log(true >= false);
+//   console.log(false == false == false == false == false == false == true);
+//   console.log(false === false)
+//   console.log(e === 'Kevin');
+//   console.log(a < b <= c);
+//   console.log(a === a != d);
+//   console.log(48 == '48');
 
-  console.log(a < b);
-  console.log(c != d);
-  console.log('Name' === 'Name');
-  console.log(true >= false);
-  console.log(false == false == false == false == false == false == true);
-  console.log(false === false)
-  console.log(e === 'Kevin');
-  console.log(a < b <= c);
-  console.log(a === a != d);
-  console.log(48 == '48');
+
+D. The farm
+1. Declare a variable animal. Set it to be either "cow" or something else.
+var x = 'animal';
+var y = 'cow'
+
+2. Write code that will print out "mooooo" if the it is equal to cow.
+var x = 'animal';
+var y = 'cow'
+
+  if (x === y) {
+    console.log('mooooo');
+}
+
+3. Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow."
+var x = 'animal';
+var y = 'cow';
+
+  if (x = y) {
+    console.log("mooooo");
+} else {
+  console.log("Hey! You're not a cow.");
+}
