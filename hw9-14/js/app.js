0e2a9ce129d8cf88a68d16a562498af278a2f86e
Mariana <marianac94@hotmail.com>
2018-09-17 09:16:46 -0500

    Functions

--- hw9-14/js/app.js
+++ hw9-14/js/app.js
@@ -307,60 +307,68 @@ console.log('weekends rock!');
 //
 //
 // H. What's in your closet
-const kristynsCloset = [
-  "left shoe",
-  "cowboy boots",
-  "right sock",
-  "GA hoodie",
-  "green pants",
-  "yellow knit hat",
-  "marshmallow peeps"
-];
-
-// Thom's closet is more complicated. Check out this nested data structure!!
-const thomsCloset = [
-  [
-    // These are Thom's shirts
-    "grey button-up",
-    "dark grey button-up",
-    "light blue button-up",
-    "blue button-up",
-  ],[
-    // These are Thom's pants
-    "grey jeans",
-    "jeans",
-    "PJs"
-  ],[
-    // Thom's accessories
-    "wool mittens",
-    "wool scarf",
-    "raybans"
-  ]
-];
-// What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
-console.log("Kristyn is rocking that " + kristynsCloset[3] + " today!");
-console.log(kristynsCloset);
-
-// Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
-kristynsCloset[5] = 'raybans';
-console.log(kristynsCloset);
-
-Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
-kristynsCloset.splice(5, 5, "stained knit hat", "marshmallow peeps");
-console.log(kristynsCloset);
-
-Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
-console.log("Thom's shirt for the day is: " + thomsCloset[0][1]);
-
-In the same way, access one item from Thom's pants array.
-console.log("Thom's pants for the day are: " + thomsCloset[1][1]);
-
-Access one item from Thom's accessories array.
-console.log("Thom's accessories for the day are: " + thomsCloset[2][2]);
-
-// Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
-console.log("Thom is looking fierce in a " + thomsCloset[0][1] + ", " + thomsCloset[1][1] + ", " + thomsCloset[2][2]);
-
-Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
-thomsCloset[1][2] = 'Footie Pijamas';
-console.log(thomsCloset[1]);
+// const kristynsCloset = [
+//   "left shoe",
+//   "cowboy boots",
+//   "right sock",
+//   "GA hoodie",
+//   "green pants",
+//   "yellow knit hat",
+//   "marshmallow peeps"
+// ];
+//
+// // Thom's closet is more complicated. Check out this nested data structure!!
+// const thomsCloset = [
+//   [
+//     // These are Thom's shirts
+//     "grey button-up",
+//     "dark grey button-up",
+//     "light blue button-up",
+//     "blue button-up",
+//   ],[
+//     // These are Thom's pants
+//     "grey jeans",
+//     "jeans",
+//     "PJs"
+//   ],[
+//     // Thom's accessories
+//     "wool mittens",
+//     "wool scarf",
+//     "raybans"
+//   ]
+// ];
+// // What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// console.log("Kristyn is rocking that " + kristynsCloset[3] + " today!");
+// console.log(kristynsCloset);
+//
+// // Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
+// kristynsCloset[5] = 'raybans';
+// console.log(kristynsCloset);
+//
+// Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
+// kristynsCloset.splice(5, 5, "stained knit hat", "marshmallow peeps");
+// console.log(kristynsCloset);
+//
+// Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirts array.
+// console.log("Thom's shirt for the day is: " + thomsCloset[0][1]);
+//
+// In the same way, access one item from Thom's pants array.
+// console.log("Thom's pants for the day are: " + thomsCloset[1][1]);
+//
+// Access one item from Thom's accessories array.
+// console.log("Thom's accessories for the day are: " + thomsCloset[2][2]);
+//
+// // Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+// console.log("Thom is looking fierce in a " + thomsCloset[0][1] + ", " + thomsCloset[1][1] + ", " + thomsCloset[2][2]);
+//
+// Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+// thomsCloset[1][2] = 'Footie Pijamas';
+// console.log(thomsCloset[1]);
+//
+//
+//
+// IV. Functions
+// Do you think you could write a function called printGreeting with a parameter name that returns a greeting with the argument interpolated into the greeting?
+// console.log(printGreeting("Slimer"));
+//
+// You think you could? I think so too. Feel free to skip this problem, because you've already done it. If you've done the problem twice, read entire problems carefully before doing them from now on.
