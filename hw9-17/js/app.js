2aa553b2df3a77b0d2a96c795d4beea2e587ec0f
Mariana <marianac94@hotmail.com>
2018-09-18 09:18:20 -0500

    object-ception

--- hw9-17/js/app.js
+++ hw9-17/js/app.js
@@ -125,44 +125,68 @@ console.log("Yey! It works!");
 
 
 // Craze Object!
-const crazyObject = {
-  taco: [
-    {
-      meat: 'steak',
-      cheese: ['panela', 'queso', 'chihuahua']
-    },
-    {
-      meat: 'chicken',
-      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
-    },
-  ],
+// const crazyObject = {
+//   taco: [
+//     {
+//       meat: 'steak',
+//       cheese: ['panela', 'queso', 'chihuahua']
+//     },
+//     {
+//       meat: 'chicken',
+//       salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+//     },
+//   ],
+//
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//
+//     characters: [
+//       {
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//     ]
+//   }
+// }
+//
+// // Use crazyObject to log the following.
+//
+// console.log(crazyObject.taco[1].salsa[5]);
+//   console.log(crazyObject.larry.quotes[0]);
+//     console.log(crazyObject.larry.characters[2].favourtieHobby);
+//       console.log(crazyObject.larry.nicknames[1]);
+//         console.log(crazyObject.larry.characters[1].name);
 
-  larry: {
-    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
 
-    characters: [
-      {
-        name: "Jeff",
-        occupation: "manager"
-      },
-      {
-        name: "funkhauser",
-        occupation: "tv dude"
-      },
-      {
-        name: "susie",
-        occupation: "jeffs wife",
-        favourtieHobby: "Swearing at Larry and Jeff"
-      },
-    ]
-  }
-}
 
-// Use crazyObject to log the following.
+// Object-ception
+        const inception = {
+           reality: {
+               dreamLayer1: {
+                   dreamLayer2: {
+                       dreamLayer3: {
+                           dreamLayer4: {
+                               dreamLayer5: {
+                                   dreamLayer6: {
+                                       limbo: "Joseph Gordon Levitt"
+                                  }
+                               }
+                           }
+                       }
+                   }
+               }
+           }
+        }
 
-console.log(crazyObject.taco[1].salsa[5]);
-  console.log(crazyObject.larry.quotes[0]);
-    console.log(crazyObject.larry.characters[2].favourtieHobby);
-      console.log(crazyObject.larry.nicknames[1]);
-        console.log(crazyObject.larry.characters[1].name);
+inception.limbo = null;
+console.log(inception);
