1d311cd5a583ce6407420f5770396f90ddefe4e7
Mariana <marianac94@hotmail.com>
2018-09-18 08:57:06 -0500

    data structures

--- hw9-17/js/app.js
+++ hw9-17/js/app.js
@@ -99,3 +99,24 @@ console.log("Yey! It works!");
         //   }
         // ];
         //   console.log(student);
+
+
+// Take it easy
+// Make an array that holds all of the colors of the rainbow.
+const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+  console.log(rainbow);
+
+// Write code that will access "blue" from the rainbow array.
+console.log(rainbow[4]);
+
+// Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+// const Mariana = [
+//   food: "Sushi",
+//   hobby: "Cooking",
+//   town: "Austin",
+//   datatype: "Objects"
+// ]
+//     console.log(Mariana);
+
+// Write code that will access your hobby from the object that you just created.
+// console.log(Mariana.hobby);
