e6d34e063ff91e1b76e4d1062e383e1af5651617
Mariana <marianac94@hotmail.com>
2018-09-18 09:14:05 -0500

    crazyObject

--- hw9-17/js/app.js
+++ hw9-17/js/app.js
@@ -101,13 +101,14 @@ console.log("Yey! It works!");
         //   console.log(student);
 
 
+
 // Take it easy
 // Make an array that holds all of the colors of the rainbow.
-const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-  console.log(rainbow);
+// const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+//   console.log(rainbow);
 
 // Write code that will access "blue" from the rainbow array.
-console.log(rainbow[4]);
+// console.log(rainbow[4]);
 
 // Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
 // const Mariana = [
@@ -120,3 +121,48 @@ console.log(rainbow[4]);
 
 // Write code that will access your hobby from the object that you just created.
 // console.log(Mariana.hobby);
+
+
+
+// Craze Object!
+const crazyObject = {
+  taco: [
+    {
+      meat: 'steak',
+      cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+      meat: 'chicken',
+      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+  ],
+
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+
+    characters: [
+      {
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
+}
+
+// Use crazyObject to log the following.
+
+console.log(crazyObject.taco[1].salsa[5]);
+  console.log(crazyObject.larry.quotes[0]);
+    console.log(crazyObject.larry.characters[2].favourtieHobby);
+      console.log(crazyObject.larry.nicknames[1]);
+        console.log(crazyObject.larry.characters[1].name);
