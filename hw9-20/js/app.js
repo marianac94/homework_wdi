95bea9fd2b7eb1b7208201dfde4843ce4cfa7bc0
Mariana <marianac94@hotmail.com>
2018-10-08 14:41:48 -0500

    hw

---
+++ hw9-20/js/app.js
@@ -0,0 +1,13 @@
+console.log('HP works!');
+
+
+// const $container = document.createClass('div');
+// div.innerText = '$container'
+
+// const $container = document.createClass('div');
+// div.appendChild(div);
+
+const $container = $('<div/>').text('This is the section');
+
+
+console.log(#container);
