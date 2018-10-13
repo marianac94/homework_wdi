fc7a6acc9ab90b17a16bb4cedcc0751ffa758c24
Mariana <marianac94@hotmail.com>
2018-10-12 22:51:07 -0500

    created models page

---
+++ hw10-12/models/shoes.js
@@ -0,0 +1,20 @@
+// storing data - the model
+const shoes = [
+  {
+    color: 'black',
+    size: 6,
+    typeOfShoe: 'high-heels boots'
+  },
+  {
+    color: 'brown',
+    size: 9,
+    typeOfShoe: 'flats'
+  },
+  {
+    color: 'white',
+    size: 8,
+    typeOfShoe: 'sneakers'
+  }
+];
+
+module.exports = shoes;
--- hw10-12/server.js
+++ hw10-12/server.js
@@ -1,6 +1,9 @@
+// required calling variables:
 const express = require('express');
 const app = express();
 
+
+// check if it works
 app.get('/', (req, res) => {
   res.send('This is the living app')
 });
@@ -16,6 +19,7 @@ app.get('/', (req, res) => {
 
 
 
+// connect with server and log in terminal
 app.listen(3000, (req, res) => {
   console.log('server is responding');
 });
