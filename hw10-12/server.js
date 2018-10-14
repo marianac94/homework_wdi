c3d7be4552f96afe98ef24834732019117d38152
Mariana <marianac94@hotmail.com>
2018-10-14 18:06:09 -0500

    post new is working

--- hw10-12/server.js
+++ hw10-12/server.js
@@ -1,7 +1,10 @@
 // required calling variables:
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const Shoes = require('./models/shoes');
+
+app.use(bodyParser.urlencoded({extended:false}));
 
 // check if it works
 app.get('/', (req, res) => {
@@ -13,17 +16,26 @@ app.get('/shoes', (req, res) => {
   res.send(Shoes)
 });
 
-// display only one single shoe from the array
-app.get('/shoes/:index', (req, res) => {
+// display only one single shoe from the array (display.ejs)
+app.get('/shoes', (req, res) => {
   console.log(req.params);
-  
+
   res.render('display.ejs', {
-    shoes: Shoes[req.params.index];
+    shoes: Shoes[req.params.index]
   });
 });
 
+// display a new shoe page route (create.ejs) it has to be above index otherwise its not going to read it
+app.get('/shoes/create', (req, res) => {
+  res.render('create.ejs')
+});
 
+app.post('/shoes', (req, res) => {
+  console.log(req.body, 'shoes data base live here');
 
+  Shoes.push(req.body);
+  res.redirect('/shoes');
+});
 
 
 
