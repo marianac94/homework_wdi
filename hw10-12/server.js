f85321b0741b315ac350f1ebd290db8e2c621c36
Mariana <marianac94@hotmail.com>
2018-10-14 16:57:42 -0500

    added views to display in the page

--- hw10-12/server.js
+++ hw10-12/server.js
@@ -1,15 +1,26 @@
 // required calling variables:
 const express = require('express');
 const app = express();
-
+const Shoes = require('./models/shoes');
 
 // check if it works
 app.get('/', (req, res) => {
   res.send('This is the living app')
 });
 
+// display all shoes in the index route
+app.get('/shoes', (req, res) => {
+  res.send(Shoes)
+});
 
-
+// display only one single shoe from the array
+app.get('/shoes/:index', (req, res) => {
+  console.log(req.params);
+  
+  res.render('display.ejs', {
+    shoes: Shoes[req.params.index];
+  });
+});
 
 
 
