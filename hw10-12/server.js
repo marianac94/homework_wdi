b45bcc1bfca20ed239d86efb974d9f9984858cdf
Mariana <marianac94@hotmail.com>
2018-10-14 21:13:02 -0500

    added delete button

--- hw10-12/server.js
+++ hw10-12/server.js
@@ -2,19 +2,23 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+// require the model from the folder (connecting it)
 const Shoes = require('./models/shoes');
 
+// middleWare- function that happens in the request from the client on the server
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
 
 // check if it works
 app.get('/', (req, res) => {
   res.send('This is the living app')
 });
 
-
-
 // Index route -shows all the shoes
 app.get('/shoes', (req, res) => {
+  console.log(req.body, 'info from shoes');
     res.render('index.ejs', {
     shoes: Shoes
   });
@@ -38,6 +42,15 @@ app.post('/shoes', (req, res) => {
     Shoes.push(req.body);
     res.redirect('/shoes');
 });
+
+// delete the shoes
+app.delete('/fruits/:index', (req, res) => {
+  console.log(req.params.index, 'request to delete this');
+    Fruits.splice(req.params.index, 1);
+    res.redirect('/shoes');
+});
+
+// edit the shoes
 
 
 
